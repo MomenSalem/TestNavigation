9313511dc228768702a916fa9e90c248e069002b
BasheerArouri1201141 <1201141@student.birzeit.edu>
2024-12-07 21:51:40 +0200

    Hi momen salem

--- app/src/main/java/com/example/testnavigation/ui/new_task/NewTaskViewModel.java
+++ app/src/main/java/com/example/testnavigation/ui/new_task/NewTaskViewModel.java
@@ -12,7 +12,7 @@ public class NewTaskViewModel extends ViewModel {
         mText = new MutableLiveData<>();
         mText.setValue("This is new Task fragment");
 
-        // Hi Momen Salem
+        // Hi Momen
     }
 
     public LiveData<String> getText() {
